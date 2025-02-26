64fd4ad13e51fbbc625e357e51dbdd86874b71f6
Lord_mahor <edvinkarter@gmail.com>
2025-02-26 04:05:19 +0400

    Update cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core'
+import * as cors from 'cors'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
-	app.enableCors()
+	app.use(
+		cors({
+			origin: '*',
+			methods: ['GET', 'POST', 'PUT', 'DELETE'],
+			allowedHeaders: ['Content-Type', 'Authorization'],
+		})
+	)
 	await app.listen(4300)
 }
 bootstrap()
